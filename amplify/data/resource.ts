32df231f56fb980cd074df943ea83dd2c1748075
JonesBenjamin <116575550+JonesBenjamin@users.noreply.github.com>
2025-03-18 14:40:29 +0000

    Added rest of the database to resource.ts
    Added Skill, AcceptedJob, Notification, Rating, Mention, Course, Subject, UserSkill, JobSkill and SubjectJob

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,7 +1,7 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
-/*== Define Job, User, and Comment Tables =====================================
-This schema defines the Job, User, and Comment tables for Amplify.
+/*== Define Job, User, Skill, AcceptedJob, Notification, Rating, Comment, Rating, Course, Subject, UserSkill, JobSkill and SubjectJob Tables =====================================
+This schema defines the Job, User, Skill, AcceptedJob, Notification, Rating, Comment, Rating, Course, Subject, UserSkill, JobSkill and SubjectJob tables for Amplify.
 =============================================================================*/
 const schema = a.schema({
   Job: a
@@ -27,12 +27,84 @@ const schema = a.schema({
     })
     .authorization((allow) => [allow.publicApiKey()]), // Adjust as needed
 
+  Skill: a
+    .model({
+      skill: a.string().required, // Unique skill
+    })
+    .authorization((allow) => [allow.publicApiKey()]), // Adjust as needed
+
+  AcceptedJob: a
+    .model({
+      jobid: a.string().required(), // Job ID
+      userid: a.string().required(), // User ID
+      applytext: a.string(), // Application Text
+    })
+    .authorization((allow) => [allow.publicApiKey()]), // Adjust as needed
+
+  Notification: a
+    .model({
+      userid: a.string().required(), // User ID
+      notiftitle: a.string(), // Notification Title
+      notifdescription: a.string(), // Notification Description
+    })
+    .authorization((allow) => [allow.publicApiKey()]), // Adjust as needed
+
+  Rating: a
+    .model({
+      jobid: a.string().required(), // Job ID
+      rateduserid: a.string().required, // Rated User ID
+      ratinguserid: a.string().required(), // Rating User ID
+      rating: a.integer().required(), // Rating (1 - 5)
+    })
+    .authorization((allow) => [allow.publicApiKey()]), // Adjust as needed
+
   Comment: a
     .model({
       jobid: a.string().required(), // Job ID
       userid: a.string().required(), // User ID
       commenttext: a.string().required(), // Comment text
       commenttime: a.timestamp().required(), // Ensure this is provided manually
+    })
+    .authorization((allow) => [allow.publicApiKey()]), // Adjust as needed
+
+  Mention: a
+    .model({
+      commentid: a.string().required(), // Comment ID
+      userid: a.string().required(), // User ID
+    })
+    .authorization((allow) => [allow.publicApiKey()]), // Adjust as needed
+
+  Course: a
+    .model({
+      coursename: a.string(), // Course Name
+    })
+    .authorization((allow) => [allow.publicApiKey()]), // Adjust as needed
+
+  Subject: a
+    .model({
+      courseid: a.string().required(), // Course ID
+      subjectname: a.string().required(), // Subject Name
+    })
+    .authorization((allow) => [allow.publicApiKey()]), // Adjust as needed
+
+  UserSkill: a
+    .model({
+      skillid: a.string().required(), // Skill ID
+      userid: a.string().required(), // User ID
+    })
+    .authorization((allow) => [allow.publicApiKey()]), // Adjust as needed
+
+  JobSkill: a
+    .model({
+      skillid: a.string().required(), // Skill ID
+      jobid: a.string().required(), // Job ID
+    })
+    .authorization((allow) => [allow.publicApiKey()]), // Adjust as needed
+
+  SubjectJob: a
+    .model({
+      subjectid: a.string().required(), // Subject ID
+      jobid: a.string().required(), // Job ID
     })
     .authorization((allow) => [allow.publicApiKey()]), // Adjust as needed
 });
