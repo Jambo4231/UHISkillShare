7667bbc4ac6bd4526e252add1a95726ecc9dfabb
richmond94 <franticrichmond94@gmail.com>
2025-03-31 10:58:26 +0100

    Potential fix for application submission

--- app/job-application/[id]/page.tsx
+++ app/job-application/[id]/page.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import { Amplify } from "aws-amplify";
-import Auth from "aws-amplify/auth";
+import Auth from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 import "../../app.css";
