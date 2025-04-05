949e99f74a0cbdce6299cd16bafe4b94b8ac218f
Jambo4231 <jsim1992@hotmail.co.uk>
2025-04-06 00:04:44 +0100

    Moved Media under the public folder so images can be accessed properly

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -21,14 +21,14 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <a href="/jobs-page">
-        <img src="../Media/logo.png" id="logo" alt="UHI Skill Share" title="UHI Skill Share" />
+        <img src="/Media/logo.png" id="logo" alt="UHI Skill Share" title="UHI Skill Share" />
       </a>
       <div className="nav-links">
         <button onClick={() => router.push("/create-new-job")}>+ New Job</button>
         <a href="/my-jobs">My Jobs</a>
         <a href="/notifications-page">Notifications</a>
         <a href="/profile">
-          <img src="../Media/DefaultProfile.png" alt="Your Profile" id="NavbarProfile" title="Your Profile" />
+          <img src="/Media/DefaultProfile.png" alt="Your Profile" id="NavbarProfile" title="Your Profile" />
         </a>
         <button onClick={handleLogout} className="logout-button">Logout</button>
       </div>
